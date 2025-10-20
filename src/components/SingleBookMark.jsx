a12de246e3dd154fc6ea7926ed9e18634d979cfc
Taanveer22 <taanveer469@gmail.com>
2025-10-21 01:10:46 +0600

    alhamdulliah full project complete

--- src/components/SingleBookMark.jsx
+++ src/components/SingleBookMark.jsx
@@ -2,8 +2,7 @@ const SingleBookMark = ({ bookmarkElement }) => {
   // console.log(bookmarkElement);
   return (
     <div>
-      <h1></h1>
-      <h1>{bookmarkElement.title}</h1>
+      <h1 className="bg-blue-500 p-1">{bookmarkElement.title}</h1>
     </div>
   );
 };
