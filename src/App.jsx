96929280f7315b0debaba3622ccc31122e5a7674
Taanveer22 <taanveer469@gmail.com>
2025-10-20 18:56:47 +0600

    alhamdulliah fake data load from public folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Blogs from "./components/Blogs";
+
+function App() {
+  return (
+    <div className="exo-font w-11/12 mx-auto">
+      <h1 className="text-4xl font-bold text-center my-5">Knowledge Cafe</h1>
+      <Blogs></Blogs>
+    </div>
+  );
+}
+
+export default App;
